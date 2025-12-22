1ac13ae1b2f94d97d186856f8aad4373df7ec1db
kadoozin <kadoozin@gmail.com>
2025-12-21 22:51:37 -0300

    feat(service): implementar camada de serviços

---
+++ src/main/java/com/example/paciente_service/service/ConsultaService.java
@@ -0,0 +1,9 @@
+package com.example.paciente_service.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ConsultaService {
+}
